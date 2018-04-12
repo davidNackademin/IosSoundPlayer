299cad2e3c989e7ed0c0e8dfd50db0c75648b9ac
David <davidsvson@gmail.com>
2018-04-12 09:43:49 +0200

    Initial Commit

---
+++ Thur12IosSound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thur12IosSound
+//
+//  Created by David Svensson on 2018-04-12.
+//  Copyright © 2018 David Svensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
